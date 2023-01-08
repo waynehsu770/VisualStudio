ee476b1f9831131f61e30033f9921e893852e4ee
waynehsu770 <waynehsu770@gmail.com>
2023-01-07 19:37:45 -0800

    Daily Report Assignment

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            int pagenumber = Convert.ToInt32(page);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string help = Console.ReadLine();
+            bool helpanswer = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int hourstoday = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
