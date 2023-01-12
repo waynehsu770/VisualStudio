86676d1612d58342b8c25060b6ed13c75f0b96a1
waynehsu770 <waynehsu770@gmail.com>
2023-01-12 15:56:26 -0800

    While Do While Practice

---
+++ WhilePractice/WhilePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhilePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int b = 0;
+            do
+            {
+                Console.WriteLine("b = " + b);
+                b++;
+            } while (b <= 10);
+            Console.Read();
+        }
+    }
+}
