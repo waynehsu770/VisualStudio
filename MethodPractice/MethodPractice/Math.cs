b9a0ddabe79e00fc59722215b22c8de6ab61248e
waynehsu770 <waynehsu770@gmail.com>
2023-01-15 23:53:27 -0800

    Method Practice

---
+++ MethodPractice/MethodPractice/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    public class Math
+    {
+        public int math_operation(int a, int b = 0) // b is the optional parameter
+        {
+            int answer = a + b;
+            return answer;
+        }
+    }
+}
---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            int answer;
+            Console.WriteLine("Please input the first number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optional: Please input the second number: ");
+            int second;
+            if (int.TryParse(Console.ReadLine(), out second)) // Checks if user input a number or not
+                answer = math.math_operation(first, second);
+            else
+                answer = math.math_operation(first); // if not only pass through the first user input 
+
+
+            Console.WriteLine("The answer is: " + answer);
+
+            Console.ReadLine();
+        }
+    }
+}
