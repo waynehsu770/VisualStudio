c6e5551035d21dd7d13a7cee7d8013789111a6c4
waynehsu770 <waynehsu770@gmail.com>
2023-01-15 23:02:41 -0800

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        public int subtraction(int data)
+        {
+            int subtraction_answer = data - 1;
+            return subtraction_answer;
+        }
+
+        public int subtraction(decimal data)
+        {
+            decimal decimal_answer = data - 2;
+            int subtraction_answer = Decimal.ToInt32(decimal_answer);
+            return subtraction_answer;
+        }
+
+        public int subtraction(string data)
+        {
+            int number = Convert.ToInt32(data);
+            int subtraction_answer = number - 4;
+            return subtraction_answer;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            int answer = math.subtraction("8");
+            Console.WriteLine(answer);
+
+            Console.ReadLine();
+        }
+    }
+}
