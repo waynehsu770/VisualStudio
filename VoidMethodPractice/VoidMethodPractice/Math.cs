72bd13f0c7ef632ecf3d1b4c81c75c18bf7ed5d8
waynehsu770 <waynehsu770@gmail.com>
2023-01-16 10:53:09 -0800

    Void Method Practice

---
+++ VoidMethodPractice/VoidMethodPractice/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodPractice
+{
+    public class Math
+    {
+        public void multiplication(int a, int b)
+        {
+            Console.WriteLine(a + " times " + b + " equals: " + (a * b));
+        }
+    }
+}
---
+++ VoidMethodPractice/VoidMethodPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math(); // Instantiate the class
+            math.multiplication(a: 4, b: 7); // Call the method, can specify parameters by name or not
+
+            Console.ReadLine();
+        }
+    }
+}
