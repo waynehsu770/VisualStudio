251e74ea2af09b3fefdc101f957636710742b8ca
waynehsu770 <waynehsu770@gmail.com>
2023-01-15 19:44:25 -0800

    Class Practice

---
+++ ClassPractice/ClassPractice/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    public class Math
+    {
+        public int multiplication(int data)
+        {
+            int multiplication_answer = data * 2;
+            return multiplication_answer;
+        }
+
+        public int division(int data)
+        {
+            int division_answer = data / 2;
+            return division_answer;
+        }
+
+        public int addition(int data)
+        {
+            int addition_answer = data + 1;
+            return addition_answer;
+        }
+    }
+}
---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number: ");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+            int answer_1 = math.multiplication(user_input);
+            int answer_2 = math.division(user_input);
+            int answer_3 = math.addition(user_input);
+            Console.WriteLine(user_input + " multiplied by 2 is: " + answer_1);
+            Console.WriteLine(user_input + " divided by 2 is: " + answer_2);
+            Console.WriteLine(user_input + " plus 1 is: " + answer_3);
+
+            Console.ReadLine();
+        }
+    }
+}
