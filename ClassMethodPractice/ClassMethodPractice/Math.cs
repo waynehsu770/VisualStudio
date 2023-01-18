599bec87c0432998fe701dda00e0018b3d45b95e
waynehsu770 <waynehsu770@gmail.com>
2023-01-18 13:55:09 +0800

    Class Method Practice

---
+++ ClassMethodPractice/ClassMethodPractice/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodPractice
+{
+    public class Math
+    {
+        public void division(int a)
+        {
+            int answer = a / 2;
+            Console.WriteLine(a + " divided by 2 equals: " + answer);
+        }
+
+        public static void addition(out int b)
+        {
+            int answer = 2 + 3;
+            b = answer;
+            Console.WriteLine(b);
+        }
+
+        public static void addition(int a, out int b)
+        {
+            int answer = a + 7;
+            b = answer;
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ ClassMethodPractice/ClassMethodPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number: ");
+            int user_number = Convert.ToInt32(Console.ReadLine());
+            math.division(user_number);
+            Math.addition(out int c);
+            Math.addition(user_number, out int d);
+
+            Console.ReadLine();
+        }
+    }
+}
