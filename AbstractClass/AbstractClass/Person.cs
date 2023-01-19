87541ab714e1e9d8ead2b753766d4c2470314f7d
waynehsu770 <waynehsu770@gmail.com>
2023-01-19 09:06:48 +0800

    Abstract Class Practice

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+
+
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee();
+            sample.firstName = "Sample";
+            sample.lastName = "Student";
+            sample.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
