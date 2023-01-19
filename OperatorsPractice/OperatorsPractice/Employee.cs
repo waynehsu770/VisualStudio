be1f46c1b0075b6cbf38d9793d9b7771e7df53f6
waynehsu770 <waynehsu770@gmail.com>
2023-01-19 21:37:43 +0800

    Operators Practice

---
+++ OperatorsPractice/OperatorsPractice/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsPractice
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int Id;
+
+        public static bool operator ==(Employee Employee_1, Employee Employee_2) // Overload the == operator
+        {
+            if(Employee_1.Id == Employee_2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee Employee_1, Employee Employee_2) // Overload the != operator 
+        {
+            if(Employee_1.Id != Employee_2.Id)
+            {
+                return true;;
+            } else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ OperatorsPractice/OperatorsPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee_1 = new Employee();
+            Employee_1.FirstName = "Sam";
+            Employee_1.LastName = "Johnson";
+            Employee_1.Id = 1;
+            Employee Employee_2 = new Employee();
+            Employee_1.FirstName = "Jessica";
+            Employee_1.LastName = "Haze";
+            Employee_1.Id = 2;
+
+            Console.WriteLine(Employee_1 == Employee_2);
+            Console.WriteLine(Employee_1 != Employee_2); 
+
+            Console.ReadLine();
+        }
+    }
+}
