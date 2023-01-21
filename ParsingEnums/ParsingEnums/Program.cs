2b83f6bb5f1396609a02d8549c82617f5b91845d
waynehsu770 <waynehsu770@gmail.com>
2023-01-21 16:26:05 +0800

    Parsing Enums

--- ParsingEnums/ParsingEnums/Program.cs
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -13,9 +13,11 @@ namespace ParsingEnums
             Console.WriteLine("Please enter the current day of the week: ");
             string user_day = Console.ReadLine();
 
+            DaysOfTheWeek user_input = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), "Monday, Tuesday");
+            Console.WriteLine(user_input);
 
+            Console.Read();
 
-            Console.ReadLine();
         }
 
         public enum DaysOfTheWeek
