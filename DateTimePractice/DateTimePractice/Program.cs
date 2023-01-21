697dd02b5c056af74fff64916093c3242c396b94
waynehsu770 <waynehsu770@gmail.com>
2023-01-21 16:26:13 +0800

    DateTime Practice

---
+++ DateTimePractice/DateTimePractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime); // Prints current time
+
+            Console.WriteLine("Please enter a number: ");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+
+            DateTime newTime = dateTime.AddHours(user_input);
+            Console.WriteLine(newTime);
+
+            Console.Read();
+        }
+    }
+}
